70931088eb476b92cd1a9221c6588a84c58fd5fd
YuriiDrahula <ydrahula@gmail.com>
2023-01-14 16:25:43 +0200

    global variable setup

--- src/main/java/com/automation/framework/xyzbank/Manager.java
+++ src/main/java/com/automation/framework/xyzbank/Manager.java
@@ -59,7 +59,7 @@ public class Manager {
 
         logger.info("Loading global variables.");
         Properties properties = new Properties();
-        FileInputStream fileInputStream = new FileInputStream(pathToGlobalVariablesRemote);
+        FileInputStream fileInputStream = new FileInputStream(pathToGlobalVariablesRemote.toString());
         properties.load(fileInputStream);
         url = properties.getProperty("url");
         firstName = properties.getProperty("firstName");
